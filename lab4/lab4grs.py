99b86bd018387e73e6219ac1dfa1fbd4465093b0
Agatha Turyahikayo <agathaturyahikayo14@gmail.com>
2019-09-23 11:13:52 -0400

    added starter code

---
+++ lab4/lab4grs.py
@@ -0,0 +1,13 @@
+'''
+starter code
+write the rest of the code to get the average of all these grades
+expected output:
+Sum of [insert num of grades] grades is: [sum]
+Average of [insert num of grades] grades is: [avg]
+'''
+
+if __name__ == "__main__":
+    grades = ["55", "25", "100", "95", "66", "14", "87", "65"]
+    grades_sum = 0
+    avg_sum = 0.0
+    num_grades = 0
