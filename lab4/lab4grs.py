2a3d89cba2696d1e9fcfff9dcb0fcfa3867d4d54
Agatha Turyahikayo <agathaturyahikayo14@gmail.com>
2019-09-24 10:57:03 -0400

    numbers are hard

--- lab4/lab4grs.py
+++ lab4/lab4grs.py
@@ -7,7 +7,7 @@ Average of [insert num of grades] grades is: [avg]
 '''
 
 if __name__ == "__main__":
-    grades = ["55", "25", "100", "95", "66", "14", "87", "65"]
+    grades = [55, 25, 100, 95, 66, 14, 87, 65]
     grades_sum = 0
     avg_sum = 0.0
     num_grades = 0
